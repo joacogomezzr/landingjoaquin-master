2d84ed4a8e683f76633337a174789aa8e2ac46ac
joacogomezzr <joaquingomezz199@gmail.com>
2024-09-26 23:25:34 -0600

    landingjoaquin

---
+++ src/component/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  
+
+}
